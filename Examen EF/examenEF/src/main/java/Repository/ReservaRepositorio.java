8f0103ae65bed2358d87d40778d1691e70e850ea
Ohiggins95 <107520806+Ohiggins95@users.noreply.github.com>
2023-11-23 09:23:19 -0500

    Delete Examen EF directory

--- Examen EF/examenEF/src/main/java/Repository/ReservaRepositorio.java
+++
@@ -1,8 +0,0 @@
-package Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import Entidades.Reserva;
-
-public interface ReservaRepositorio extends JpaRepository<Reserva, Long> {
-}
--- Examen EF/examenEF/src/main/java/Repository/UsuarioRepositorio.java
+++
@@ -1,8 +0,0 @@
-package Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import Entidades.Usuario;
-
-public interface UsuarioRepositorio extends JpaRepository<Usuario, Long> {
-}
